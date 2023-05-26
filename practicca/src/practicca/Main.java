304891c31fba9237b40168dc6c371bc99102a20b
DannyCeacFp <danny.cuenca@alu.ceacfp.com>
2023-05-26 10:38:16 +0200

    cambiando

--- practicca/src/practicca/Main.java
+++ practicca/src/practicca/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String saludo = "hola a ti";
+		String saludo = "";
 		String saludo2 = "hola a tis";
 		String saludo3 = "hola a ti";
 
