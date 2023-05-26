09cbc8e4c2ab87fca24232f3a396370e524dcbf1
DannyCeacFp <danny.cuenca@alu.ceacfp.com>
2023-05-26 10:19:20 +0200

    cambiando saludo 2

--- practicca/src/practicca/Main.java
+++ practicca/src/practicca/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String saludo = "hola a ti";
-		String saludo2 = "hola todos";
+		String saludo2 = "hola a tis";
 		String saludo3 = "hola todos";
 		String saludo4 = "hola todos";
 		
